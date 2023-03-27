69ad4792b9d16e17a5b93ff27219dc3c2570d84f
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-03-27 15:13:13 +0200

    Also copy other output information

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -1,3 +1,3 @@
 docker run --rm -it -v $(pwd)/input:/input -v $(pwd)/output:/output node:14-bullseye /bin/bash /input/build_frontend.sh
 
-cat output/{runtime,polyfills,main}.js > "../../src/static/cee/cedar-embeddable-editor.js"
+cat output/{runtime,polyfills,main}.js > "../src/static/cee/cedar-embeddable-editor.js"
