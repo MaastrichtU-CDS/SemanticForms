a231abbee6693212271a4603e7b3f9fb8fe5f6e9
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-03-27 20:16:04 +0200

    Fixed build version

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -1,9 +1,11 @@
+NODE_VERSION=14
 rm -Rf $(pwd)/output
 
-docker run --rm -it -v $(pwd)/input:/input -v $(pwd)/output:/output node:14-bullseye /bin/bash /input/build_frontend.sh
-
-docker run --rm -it -v $(pwd)/output:/output --workdir /output node:14-bullseye npm install @webcomponents/webcomponentsjs
+docker run --rm -it -v $(pwd)/input:/input -v $(pwd)/output:/output node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
 
 cat output/{runtime,polyfills,main}.js > "../src/static/cee/cedar-embeddable-editor.js"
+
+docker run --rm -it -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
+
 cp -R output/node_modules ../src/static/cee/node_modules
 cp output/{MaterialIcons-*,styles.css} ../src/static/cee/
