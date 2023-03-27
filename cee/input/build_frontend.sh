db7f97d68ffc627f45f6f1cb4ba1d131496d4b0e
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-03-27 20:16:28 +0200

    Now without the file override (not needed anymore)

--- cee/input/build_frontend.sh
+++ cee/input/build_frontend.sh
@@ -1,4 +1,4 @@
-version='2.6.21'
+version='2.6.18'
 
 mkdir /build
 cd /build
@@ -12,14 +12,13 @@ rm -Rf release-*.zip
 curl -L -o release-$version.zip https://github.com/metadatacenter/cedar-embeddable-editor/archive/refs/tags/release-$version.zip
 unzip release-$version.zip
 
-# Override files needed to work in our situation
-cp app.component.ts cedar-embeddable-editor-release-$version/src/app/app.component.ts
-cp app.module.ts cedar-embeddable-editor-release-$version/src/app/app.module.ts
+# # Override files needed to work in our situation
+# cp app.component.ts cedar-embeddable-editor-release-$version/src/app/app.component.ts
+# cp app.module.ts cedar-embeddable-editor-release-$version/src/app/app.module.ts
 
 # Build project
 cd cedar-embeddable-editor-release-$version
 sed -i "/this.messageHandlerService.traceObject/ a window.location.href = '\\/';" src/app/modules/shared/components/cedar-data-saver/cedar-data-saver.component.ts
-npm install @angular/cli
 npm install
 # node_modules/@angular/cli/bin/ng build --configuration production --baseHref="./static/cee/"
 node_modules/@angular/cli/bin/ng build --configuration production --output-hashing=none --baseHref="./static/cee/"
