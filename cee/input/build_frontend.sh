1160f013dff64a02c533e7f13023557364c76875
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-03-27 15:00:43 +0200

    Build in docker container

--- cee/input/build_frontend.sh
+++ cee/input/build_frontend.sh
@@ -1,13 +1,9 @@
 version='2.6.18'
-os=$(uname -s)
 
-sed=sed
-if [[ $os -eq "Darwin" ]]; then
-    sed=gsed
-fi
-echo $sed
+mkdir /build
+cd /build
 
-cd "$( dirname "${BASH_SOURCE[0]}" )"
+cp /input/*.ts ./
 
 rm -Rf cedar-embeddable-editor*
 rm -Rf release-*.zip
@@ -22,15 +18,11 @@ cp app.module.ts cedar-embeddable-editor-release-$version/src/app/app.module.ts
 
 # Build project
 cd cedar-embeddable-editor-release-$version
-$sed -i "/this.messageHandlerService.traceObject/ a window.location.href = '\\/';" src/app/modules/shared/components/cedar-data-saver/cedar-data-saver.component.ts
+sed -i "/this.messageHandlerService.traceObject/ a window.location.href = '\\/';" src/app/modules/shared/components/cedar-data-saver/cedar-data-saver.component.ts
 npm install @angular/cli
 npm install
 # node_modules/@angular/cli/bin/ng build --configuration production --baseHref="./static/cee/"
 node_modules/@angular/cli/bin/ng build --configuration production --output-hashing=none --baseHref="./static/cee/"
 
-# Copy to src static
-rm -R ../../src/static/cee
-mkdir -p ../../src/static/cee
-# cp -R dist/cedar-embeddable-editor/* ../../src/static/cee/
-# cp ../../src/static/cee/index.html ../../src/templates/cee.html
-cat dist/cedar-embeddable-editor/{runtime,polyfills,main}.js > "../../src/static/cee/cedar-embeddable-editor.js"
+# copy output
+cp -R ./dist/cedar-embeddable-editor/* /output
