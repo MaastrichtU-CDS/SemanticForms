1bf38d8d307299d57ab4bd81f33cf6c15f03da7c
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-06-09 11:15:46 +0200

    Adaptations to relative paths

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -6,9 +6,9 @@ rm -Rf $(pwd)/output
 
 docker run --rm -v $(pwd)/input:/input -v $(pwd)/output:/output -v node-$NODE_VERSION-cedar-build:/build node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
 
-cat output/{runtime,polyfills,main}.js > "../src/static/cee/cedar-embeddable-editor.js"
+cat $(pwd)/output/{runtime,polyfills,main}.js > $(pwd)/../src/static/cee/cedar-embeddable-editor.js
 
 docker run --rm -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
 
-cp -R output/node_modules ../src/static/cee/node_modules
-cp output/{MaterialIcons-*,styles.css} ../src/static/cee/
+cp -R $(pwd)/output/node_modules ../src/static/cee/node_modules
+cp $(pwd)/output/{MaterialIcons-*,styles.css} $(pwd)/../src/static/cee/
