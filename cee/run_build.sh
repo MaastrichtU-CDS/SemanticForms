cfc5c5322135f470dbdf794c2107d58ee5ce0031
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-06-09 11:11:33 +0200

    See if the non-interactive mode works

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -4,11 +4,11 @@ docker volume create node-$NODE_VERSION-cedar-build
 
 rm -Rf $(pwd)/output
 
-docker run --rm -it -v $(pwd)/input:/input -v $(pwd)/output:/output -v node-$NODE_VERSION-cedar-build:/build node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
+docker run --rm -v $(pwd)/input:/input -v $(pwd)/output:/output -v node-$NODE_VERSION-cedar-build:/build node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
 
 cat output/{runtime,polyfills,main}.js > "../src/static/cee/cedar-embeddable-editor.js"
 
-docker run --rm -it -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
+docker run --rm -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
 
 cp -R output/node_modules ../src/static/cee/node_modules
 cp output/{MaterialIcons-*,styles.css} ../src/static/cee/
