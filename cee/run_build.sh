f0577b427da237705ad66d6fdb3610f6c8875f25
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-06-09 11:18:57 +0200

    Check output of actions

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -5,7 +5,7 @@ docker volume create node-$NODE_VERSION-cedar-build
 rm -Rf $(pwd)/output
 
 docker run --rm -v $(pwd)/input:/input -v $(pwd)/output:/output -v node-$NODE_VERSION-cedar-build:/build node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
-
+ls $(pwd)/output
 cat $(pwd)/output/{runtime,polyfills,main}.js > $(pwd)/../src/static/cee/cedar-embeddable-editor.js
 
 docker run --rm -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
