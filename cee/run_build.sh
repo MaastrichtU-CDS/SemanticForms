50304965c8ed9aea6d3a5e947a6928b5c5e6f089
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-06-14 15:41:37 +0200

    create dir before first ocntent is added

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -6,11 +6,10 @@ rm -Rf $(pwd)/output
 
 docker run --rm -v $(pwd)/input:/input -v $(pwd)/output:/output -v node-$NODE_VERSION-cedar-build:/build node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
 
-cat $(pwd)/output/cedar-embeddable-editor.js > $(pwd)/../src/static/cee/cedar-embeddable-editor.js
+mkdir -p $(pwd)/../src/static/cee/node_modules
+cp $(pwd)/output/cedar-embeddable-editor.js > $(pwd)/../src/static/cee/cedar-embeddable-editor.js
 
 docker run --rm -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
-
-mkdir -p $(pwd)/../src/static/cee/node_modules
 
 cp -R $(pwd)/output/node_modules $(pwd)/../src/static/cee
 cp $(pwd)/output/styles.css $(pwd)/../src/static/cee/
