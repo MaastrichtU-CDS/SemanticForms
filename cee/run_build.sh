c64a09c62223686c2d7a9318c5b016b6d0dfa0df
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-06-16 13:23:46 +0200

    Added fix for font types

--- cee/run_build.sh
+++ cee/run_build.sh
@@ -7,7 +7,8 @@ rm -Rf $(pwd)/output
 docker run --rm -v $(pwd)/input:/input -v $(pwd)/output:/output -v node-$NODE_VERSION-cedar-build:/build node:$NODE_VERSION-bullseye /bin/bash /input/build_frontend.sh
 
 mkdir -p $(pwd)/../src/static/cee/node_modules
-cp $(pwd)/output/cedar-embeddable-editor.js $(pwd)/../src/static/cee/cedar-embeddable-editor.js
+# Add static folder prefix to load font type correctly in javascript
+gsed 's/MaterialIcons/static\/cee\/MaterialIcons/g' output/cedar-embeddable-editor.js > $(pwd)/../src/static/cee/cedar-embeddable-editor.js
 
 docker run --rm -v $(pwd)/output:/output --workdir /output node:$NODE_VERSION-bullseye npm install @webcomponents/webcomponentsjs
 
