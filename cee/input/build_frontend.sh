0cf769d2f515e01c1a6499e210ca3d671ae490c6
Johan van Soest <j.vansoest@maastrichtuniversity.nl>
2023-10-30 13:58:47 +0100

    reinstated original version

--- cee/input/build_frontend.sh
+++ cee/input/build_frontend.sh
@@ -8,16 +8,16 @@ rm -Rf cedar-embeddable-editor*
 rm -Rf release-*.zip
 
 # Download and unzip release version of CEE
-# curl -L -o release-$version.zip https://github.com/metadatacenter/cedar-embeddable-editor/archive/refs/tags/release-$version.zip`
-curl -L -o release-$version.zip https://github.com/metadatacenter/cedar-embeddable-editor/archive/refs/heads/develop.zip
+curl -L -o release-$version.zip https://github.com/metadatacenter/cedar-embeddable-editor/archive/refs/tags/release-$version.zip
+# curl -L -o release-$version.zip https://github.com/metadatacenter/cedar-embeddable-editor/archive/refs/heads/develop.zip
 unzip release-$version.zip
 
 # # Override files needed to work in our situation
-# cp app.component.ts cedar-embeddable-editor-release-$version/src/app/app.component.ts
-# cp app.module.ts cedar-embeddable-editor-release-$version/src/app/app.module.ts
+cp app.component.ts cedar-embeddable-editor-release-$version/src/app/app.component.ts
+cp app.module.ts cedar-embeddable-editor-release-$version/src/app/app.module.ts
 
 # Build project
-cd cedar-embeddable-editor-develop
+cd cedar-embeddable-editor-$version
 sed -i "/this.messageHandlerService.traceObject/ a window.location.href = '\\/';" src/app/modules/shared/components/cedar-data-saver/cedar-data-saver.component.ts
 npm install --no-audit
 # node_modules/@angular/cli/bin/ng build --configuration production --baseHref="./static/cee/"
